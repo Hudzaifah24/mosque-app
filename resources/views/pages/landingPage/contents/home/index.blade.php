aafa228e9ef550047d0ae370b521a0a21ae18a1a
Hudzaifah <hudz1357@gmail.com>
2022-12-15 18:55:14 +0700

    kepo commit

--- resources/views/pages/landingPage/contents/home/index.blade.php
+++ resources/views/pages/landingPage/contents/home/index.blade.php
@@ -2,71 +2,41 @@
 
 @section('content')
     <main class="w-full flex-grow pb-6">
-        <div class="splide" role="group" aria-label="Splide Basic HTML Example">
-            <div class="splide__arrows">
-                <button class="splide__arrow splide__arrow--prev">
-                    <i class="fas fa-chevron-right text-xl"></i>
-                </button>
-                <button class="splide__arrow splide__arrow--next">
-                    <i class="fas fa-chevron-right text-xl"></i>
-                </button>
-          </div>
-            <div class="splide__track">
-                <ul class="splide__list">
-                    <li class="splide__slide relative">
-                        <img class="w-full h-full bg-cover" src="{{ asset('image/slide1.jpg') }}" alt="slide 1">
-                        <div class="absolute top-0 w-full h-full bg-black opacity-75"></div>
-                        <div class="absolute top-0 w-full h-full flex justify-center items-center flex-col">
-                            <div class="text-center rounded mt-2">
-                                <div class="flex flex-row px-4">
-                                    <p class="text-white font-black py-3 text-3xl" id="jam">0</p>
-                                    <p class="text-white font-black py-3 text-3xl">&nbsp;:&nbsp;</p>
-                                    <p class="text-white font-black py-3 text-3xl" id="menit">0</p>
-                                    <p class="text-white font-black py-3 text-3xl">&nbsp;:&nbsp;</p>
-                                    <p class="text-white font-black py-3 text-3xl" id="detik">0</p>
-                                </div>
-                            </div>
-                            <p class="text-white text-center uppercase font-black text-xl text-ellipsis">Selamat datang</p>
-                            <p class="text-white text-center uppercase font-black text-3xl text-ellipsis">MASJID AL-IKHLAS</p>
-                            <div class="bg-white text-center rounded mt-3">
-                                <div class="flex flex-row px-4">
-                                    <p class="text-black font-black py-3 text-xl" id="pray"></p>
-                                </div>
-                            </div>
+        <ul>
+            <li class="relative">
+                <img class="w-full h-full bg-cover" src="{{ asset('image/slide1.jpg') }}" alt="slide 1">
+                <div class="absolute top-0 w-full h-full bg-black opacity-75"></div>
+                <div class="absolute top-0 w-full h-full flex justify-center items-center flex-col">
+                    <div class="text-center rounded mt-2">
+                        <div class="flex flex-row px-4">
+                            <p class="text-white font-black py-3 text-3xl" id="jam">0</p>
+                            <p class="text-white font-black py-3 text-3xl">&nbsp;:&nbsp;</p>
+                            <p class="text-white font-black py-3 text-3xl" id="menit">0</p>
+                            <p class="text-white font-black py-3 text-3xl">&nbsp;:&nbsp;</p>
+                            <p class="text-white font-black py-3 text-3xl" id="detik">0</p>
                         </div>
-                    </li>
-                    <li class="splide__slide relative">
-                        <img class="w-full h-full bg-cover" src="{{ asset('image/slide2.jpg') }}" alt="slide 2">
-                        <div class="absolute top-0 w-full h-full bg-black opacity-75"></div>
-                        <div class="absolute top-0 w-full h-full flex justify-center items-center flex-col">
-                            <p class="text-white text-center uppercase font-black text-xl text-ellipsis">Selamat datang</p>
-                            <p class="text-white text-center uppercase font-black text-3xl text-ellipsis">MASJID AL-IKHLAS</p>
-                            <div class="bg-white text-center rounded mt-2">
-                                <p class="text-black font-bold bg-blue-300 rounded-t py-1 px-2"></p>
-                                <p class="text-black font-black py-3 px-5 text-3xl">12:00 WIB</p>
-                            </div>
+                    </div>
+                    <p class="text-white text-center uppercase font-black text-xl text-ellipsis">Selamat datang</p>
+                    <p class="text-white text-center uppercase font-black text-3xl text-ellipsis">MASJID AL-IKHLAS</p>
+                    <div class="bg-white text-center rounded mt-3">
+                        <div class="flex flex-row px-4">
+                            <p class="text-black font-black py-3 text-xl" id="pray"></p>
                         </div>
-                    </li>
-                    <li class="splide__slide relative">
-                        <img class="w-full h-full bg-cover" src="{{ asset('image/slide3.jpg') }}" alt="slide 3">
-                        <div class="absolute top-0 w-full h-full bg-black opacity-75"></div>
-                        <div class="absolute top-0 w-full h-full flex justify-center items-center flex-col">
-                            <p class="text-white text-center uppercase font-black text-xl text-ellipsis">Selamat datang</p>
-                            <p class="text-white text-center uppercase font-black text-3xl text-ellipsis">MASJID AL-IKHLAS</p>
-                            <div class="bg-white text-center rounded mt-2">
-                                <p class="text-black font-bold bg-blue-300 rounded-t py-1 px-2">Kajian Rutin</p>
-                                <p class="text-black font-black py-1 px-2">Fiqh Muamalat : 05:00 WIB</p>
-                                <p class="text-black font-black py-1 px-2">Aqidah Usul : 19:00 WIB</p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
 
         <div class="bg-gray-300 rounded-b m-2 flex flex-col lg:flex-row justify-center lg:justify-between">
             <div class="p-5 w-full lg:w-1/2">
                 <p class="text-center pb-6 text-xl font-bold" id="estimasi"></p>
+
+                <div class="w-full mb-3">
+                    <input id="input" list="kota" value="1204" class="py-2 px-3 rounded">
+                    <datalist id="kota">
+                    </datalist>
+                    <button onclick="btnkota()" class="py-2 px-3 rounded bg-white">></button>
+                </div>
 
                 <div class="bg-white mx-auto overflow-auto">
                     <table class="min-w-full leading-normal">
@@ -118,25 +88,11 @@
     </main>
 @endsection
 
-@push('style')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide-core.min.css">
-@endpush
-
 @push('script')
-    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js" integrity="sha512-6+YN/9o9BWrk6wSfGxQGpt3EUK6XeHi6yeHV+TYD2GR0Sj/cggRpXr1BrAQf0as6XslxomMUxXp2vIl+fv0QRA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script>
-        // Slide
-        var splide = new Splide( '.splide', {
-            type: 'loop',
-        } );
-        splide.mount();
-
         // JAM
-        window.setTimeout(time, 1000);
-
         function time() {
             var clock = new Date();
             let hour = clock.getHours();
@@ -161,6 +117,36 @@
             document.getElementById('detik').innerHTML = second;
 
             setTimeout(time, 1000);
+        }
+
+        // Kota
+        kota();
+
+        let res = 1204;
+
+        function btnkota() {
+            let input = document.getElementById("input").value;
+            res = input;
+
+            sholat(res);
+        }
+
+        function kota() {
+            fetch('https://api.myquran.com/v1/sholat/kota/semua')
+            .then((response) => response.json())
+            .then((data) => {
+                let select = '';
+
+                data.forEach((kota) => {
+                    select += `
+                        <option value="${kota.id}">
+                            ${kota.lokasi}
+                        </option>
+                    `;
+                });
+
+                document.getElementById('kota').innerHTML = select;
+            });
         }
 
         // Kajian
@@ -208,26 +194,24 @@
         }
 
         // Jadwal Sholat
-        window.setTimeout(sholat, 1000);
-
-        function sholat() {
+        function sholat(kota = res) {
             let tanggal = new Date().getDate();
             let bulan = new Date().getMonth();
             let tahun = new Date().getFullYear();
 
-            adzan(tanggal, bulan, tahun);
-            nextPray(tanggal, bulan, tahun);
+            adzan(tanggal, bulan, tahun, kota);
+            nextPray(tanggal, bulan, tahun, kota);
 
-            fetch('https://api.myquran.com/v1/sholat/jadwal/1204/'+tahun+'/'+bulan+'/'+tanggal)
+            fetch('https://api.myquran.com/v1/sholat/jadwal/'+kota+'/'+tahun+'/'+bulan+'/'+tanggal)
             .then((response) => response.json())
             .then((data) => {
                 let sholat = '';
-                let estimasi = 'Jadwal Sholat '+data.data.lokasi+' '+data.data.daerah;
+                let estimasi = `Jadwal Sholat ${data.data.lokasi} ${data.data.daerah}, <span class="text-white bg-blue-500 rounded py-1 px-2">${data.data.jadwal.tanggal}</span>`;
 
                 sholatObj = data.data.jadwal;
 
                 for (const key in sholatObj) {
-                    if (key !== 'date') {
+                    if (key !== 'date' && key !== 'tanggal') {
                         sholat += `
                             <tr>
                                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -251,7 +235,7 @@
         }
 
         // Alarm Adzan
-        function adzan(tanggal, bulan, tahun) {
+        function adzan(tanggal, bulan, tahun, kota) {
             let date = new Date();
             let hour = date.getHours();
             let minute = date.getMinutes();
@@ -271,7 +255,7 @@
             }
             let clock = `${hour}:${minute}:${second}`;
 
-            fetch('https://api.myquran.com/v1/sholat/jadwal/1204/'+tahun+'/'+bulan+'/'+tanggal)
+            fetch('https://api.myquran.com/v1/sholat/jadwal/'+kota+'/'+tahun+'/'+bulan+'/'+tanggal)
             .then((response) => response.json())
             .then((data) => {
                 let sholatObj = data.data.jadwal;
@@ -284,10 +268,10 @@
                             src: ["{{ asset('audio/adzan1.mp3') }}"],
                             loop: false,
                             autoplay: true,
-                            onend: function() {
-                                console.log('Finised!');
-                                confirm("Waktu Iqomah 10 detik lagi");
-                            }
+                            // onend: function() {
+                            //     console.log('Finised!');
+                            //     confirm("Waktu Iqomah 10 detik lagi");
+                            // }
                         });
 
                         audio.once('load', function() {
@@ -300,7 +284,7 @@
         }
 
         // Next prayer
-        function nextPray(tanggal, bulan, tahun) {
+        function nextPray(tanggal, bulan, tahun, kota) {
             let date = new Date();
             let hour = date.getHours();
             let minute = date.getMinutes();
@@ -320,20 +304,21 @@
             }
             let clock = `${hour}:${minute}`;
 
-            // let ex = {
-            //     imsak: "04:00",
-            //     subuh: "04:10",
-            //     dzuhur: "05:00",
-            //     ashar: "15:01",
-            //     maghrib: "17:02",
-            //     isya: "18:30",
-            //     example: "14:16",
-            // }
 
-            fetch('https://api.myquran.com/v1/sholat/jadwal/1204/'+tahun+'/'+bulan+'/'+tanggal)
+            fetch('https://api.myquran.com/v1/sholat/jadwal/'+kota+'/'+tahun+'/'+bulan+'/'+tanggal)
             .then((response) => response.json())
             .then((data) => {
                 let sholatObj = data.data.jadwal;
+
+                // let ex = {
+                //     imsak: "04:00",
+                //     subuh: "04:10",
+                //     dzuhur: "05:00",
+                //     ashar: "15:01",
+                //     maghrib: "17:02",
+                //     isya: "18:30",
+                //     example: "15:40",
+                // }
 
                 let nextPrayer = "";
 
@@ -341,22 +326,30 @@
                     let potong = sholatObj[key].split(":");
                     let [h, m] = potong;
 
-                    if (h > hour) {
+                    if (h >= hour) {
                         let name = key;
                         let waktu = sholatObj[key];
+                        if (waktu == clock) {
+                            if (h > hour) {
+                                nextPrayer = `Akan Tiba Sholat ${name} <span class="text-white bg-blue-500 py-1 px-2 rounded">${waktu}</span>`;
+                                break;
+                            }
+                            break;
+                        }
 
-                        nextPrayer = `Akan Tiba Sholat ${name} <span class="text-white bg-green-500 py-1 px-2 rounded">${waktu}</span>`
+                        nextPrayer = `Akan Tiba Sholat ${name} <span class="text-white bg-blue-500 py-1 px-2 rounded">${waktu}</span>`;
+
                         break;
                     }
                 }
 
                 document.getElementById('pray').innerHTML = nextPrayer;
-                console.log(nextPrayer)
             })
             .catch((err) => console.log(err));
         }
 
         function reload(){
+            time();
             sholat();
             setTimeout(reload, 1000);
         };
